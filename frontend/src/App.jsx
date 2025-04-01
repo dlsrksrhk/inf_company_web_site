81190dafcef8f4693f7a98413ac637130b151965
sweet.dh <dlsrksrhk@gmail.com>
2025-04-01 11:01:08 +0900

    하단바 완료

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
-import { BrowserRouter } from "react-router-dom";
-import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Footer />
     </BrowserRouter>
   );
 }
